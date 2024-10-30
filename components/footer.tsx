3fed3283a69bf926ef8c62dca213b8784f87a919
Rahul Sharma <77871021+rahul-sharma-cs@users.noreply.github.com>
2024-10-30 00:17:43 -0400

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,10 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
+    <footer className="mb-5 mt-5 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
         &copy; 2024 Rahul Sharma. All rights reserved.
       </small>
-      <p className="text-xs">
-        Built with React & Next.js, TypeScript, Tailwind CSS, Framer Motion.
-      </p>
     </footer>
   );
 }
